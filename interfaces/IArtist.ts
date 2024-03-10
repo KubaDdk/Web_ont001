bd46f7a78c72d1f0fbfd2855d0a4cb1a763fb382
Jakub Dudek <jdudek@3p.eu>
2024-03-10 20:56:15 +0100

    add interfaces

---
+++ interfaces/IArtist.ts
@@ -0,0 +1,21 @@
+type RecordLabel = {
+    id: string;
+    name: string;
+    labelLogoUrl: string;
+    foundedYear: number;
+    founder: string;
+    headquarters: string;
+  };
+  
+  type Artist = {
+    id: string;
+    name: string;
+    description: string;
+    age: number;
+    isActive: boolean;
+    birthDate: string;
+    imageUrl: string;
+    genre: string;
+    instruments: string[];
+    recordLabel: RecordLabel;
+  };
